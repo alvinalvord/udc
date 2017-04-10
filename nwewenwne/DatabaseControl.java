7405265fea79f5e7da6d8539910310c5386432c6
alvinalvord <jasper_pillejera@dlsu.edu.ph>
2017-04-11 03:24:17 +0800

    sad

---
+++ nwewenwne/DatabaseControl.java
@@ -0,0 +1,7 @@
+public interface DatabaseControl {
+	public void deleteRow (int id);
+	public void markDone (int id);
+	public void createUpdate (int id, String... param);
+	public int createInsert (String... param);
+	public java.sql.ResultSet getUpdates (String id, int y, int m, int d);
+}
