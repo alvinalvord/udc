9ce3d6a3e14719d2fa01b0336a31c222fb661d91
Luigi Acorda <luigi_acorda@dlsu.edu.ph>
2017-04-14 22:08:55 +0800

    git

---
+++ ver1.00000/ClientController.java
@@ -0,0 +1,38 @@
+import java.sql.*;
+
+import javafx.stage.*;
+
+public class ClientController implements DatabaseControl{
+
+	private MainController mc;
+	private ClinicView cv;
+	
+	public ClientController (MainController mc) {
+		this.mc = mc;
+	}
+	
+	public Stage getMainStage () {
+		return mc.getStage ();
+	}
+
+	public void deleteRow(int id) {
+		
+	}
+
+	public void createUpdate(int id, String... param) {
+		
+	}
+
+	public int createInsert(String... param) {
+		return 0;
+	}
+
+	public int getAccessLevel(int id) {
+		return 0;
+	}
+
+	public ResultSet getUpdates(String id, int y, int m, int d) {
+		return null;
+	}
+
+}
---
+++ ver1.00000/SecretaryController.java
@@ -0,0 +1,36 @@
+import java.sql.*;
+
+import javafx.stage.*;
+
+public class SecretaryController implements DatabaseControl {
+	private MainController mc;
+	private ClinicView cv;
+	
+	public SecretaryController (MainController mc) {
+		this.mc = mc;
+	}
+	
+	public Stage getMainStage () {
+		return mc.getStage ();
+	}
+
+	public void deleteRow(int id) {
+		
+	}
+	
+	public void createUpdate(int id, String... param) {
+		
+	}
+
+	public int createInsert(String... param) {
+		return 0;
+	}
+
+	public int getAccessLevel(int id) {
+		return 0;
+	}
+
+	public ResultSet getUpdates(String id, int y, int m, int d) {
+		return null;
+	}
+}
