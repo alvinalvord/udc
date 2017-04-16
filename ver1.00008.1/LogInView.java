3761ade2ce29c81ed3f38bb57407125c3eb7dae0
alvinalvord <jasper_pillejera@dlsu.edu.ph>
2017-04-16 19:53:36 +0800

    sad

---
+++ ver1.00008.1/LogInView.java
@@ -0,0 +1,74 @@
+import javafx.geometry.*;
+import javafx.scene.control.*;
+import javafx.scene.layout.*;
+
+public class LogInView extends BorderPane {
+	private LogInController lic;
+	private VBox loginBox;
+		private HBox usernameBox;
+			private Label username;
+			private TextField usernameField;
+		private HBox passwordBox;
+			private Label password;
+			private TextField passwordField;
+		private Button login;
+	
+	public LogInView (LogInController lic) {
+		super ();
+		
+		this.lic = lic;
+		
+		setMaxSize (Double.MAX_VALUE, Double.MAX_VALUE);
+		
+		initLogInView ();
+		initHandlers ();
+		
+		setCenter (loginBox);
+	}
+	
+	private void initLogInView () {		
+		loginBox = new VBox (20);
+		loginBox.setAlignment (Pos.CENTER);
+		loginBox.setId("LoginBox");
+		
+			usernameBox = new HBox (10);
+			usernameBox.setAlignment (Pos.CENTER);
+			
+				username = new Label ();
+				username.setText ("Username:");
+				username.setId ("DefaultLabel");
+				usernameField = new TextField ();
+				usernameField.setText("");
+				usernameField.setId("TextField");
+				
+			usernameBox.getChildren ().addAll (username, usernameField);
+			
+			passwordBox = new HBox (10);
+			passwordBox.setAlignment (Pos.CENTER);
+			
+				password = new Label ();
+				password.setText ("Password:");
+				password.setId ("DefaultLabel");
+				passwordField = new TextField ();
+				passwordField.setText ("");
+				passwordField.setId("TextField");
+				
+			passwordBox.getChildren ().addAll (password, passwordField);
+			
+			login = new Button ();
+			login.setText("Login");
+			login.getStyleClass ().add ("ButtonSave");
+			
+		loginBox.getChildren ().addAll (usernameBox, passwordBox, login);
+	}
+	
+	private void initHandlers () {
+		login.setOnAction(e -> {
+			if (lic.validateUser (usernameField.getText (), passwordField.getText ()))
+				lic.changeControl (1);
+			
+			usernameField.setText ("");
+			passwordField.setText ("");
+		});
+	}
+}
